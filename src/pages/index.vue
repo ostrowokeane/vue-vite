d300a3a0e67f9907aee8f354ceb348c483be92c1
Danila Yermolchev <ermolchevddd@gmail.com>
2024-08-08 22:38:35 +0500

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-layout>
+    <v-main>
+      <div class="over-transition">
+        <router-view v-slot="{ Component, route }">
+          <transition-group :name="transitionName">
+            <div :key="route.path" class="in-transition">
+              <component :key="route.path" :is="Component" />
+            </div>
+          </transition-group>
+        </router-view>
+      </div>
+    </v-main>
+
+    <v-bottom-navigation color="default">
+      <v-btn color="default">
+        <v-icon>mdi-glass-cocktail</v-icon>
+        Меню
+      </v-btn>
+
+      <v-btn color="default">
+        <v-icon>mdi-list-status</v-icon>
+        Заказы
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { router, isManual } from "@/app/router/router";
+import { isIOS } from "@vueuse/core";
+import { watch } from "vue";
+import { ref } from "vue";
+
+/**
+ * Помимо fetch еще и удаляет из заказа элементы, которые возможно закончились
+ */
+
+const transitionName = ref("scale-fade");
+
+const getTransitionName = (to: any, from: any) => {
+  if (to.meta.section !== from.meta.section) return "scale-fade";
+
+  const toDepth = (to.meta.depth as number) || 1,
+    fromDepth = (from.meta.depth as number) || 1;
+
+  return toDepth === fromDepth
+    ? "scale-fade"
+    : toDepth > fromDepth
+    ? "slide-forward"
+    : "slide-backword";
+};
+
+watch(
+  () => router.currentRoute.value,
+  isIOS
+    ? async (to, from) => {
+        transitionName.value = isManual.value
+          ? getTransitionName(to, from)
+          : "none";
+      }
+    : async (to, from) => {
+        transitionName.value = getTransitionName(to, from);
+      }
+);
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+    },
+    colors: {
+      white: '#fff',
+      btnColor: '#007AFF',
+    }
+  },
+  plugins: [],
+}
+
